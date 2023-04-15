786c210e61d1710390032fe400950efc8e1b981e
Samita-1998 <samita91998@gmail.com>
2023-04-16 00:15:27 +0530

    Done

---
+++ Assignment6/Q3/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template : '<style> h1 {color : blue} </style> <h1> Marvellous Infosystem </h1> <h1> By Piyush Khairnar </h1> ',
+  styles : []
+})
+export class AppComponent {
+  title = 'Assignment6';
+
+
+}
